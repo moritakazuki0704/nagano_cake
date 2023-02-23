2896b52048a8015f18e3a66709ccf26214fe2b8f
EC2 Default User <ec2-user@ip-172-31-33-118.ec2.internal>
2023-02-23 00:59:48 +0000

    [add] create admin_customer create views/admin/customers/index.html.erb update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
     get 'genres/index'
     get 'genres/edit'
   end
@@ -16,5 +21,6 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :items,except:[:destroy]
     resources :genres,except:[:new,:show,:destroy]
+    resources :customers,except:[:new,:create,:destroy]
   end
 end
