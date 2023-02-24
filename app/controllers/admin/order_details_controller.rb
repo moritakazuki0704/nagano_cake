f8807924912b3bb6fc0beb744fd93d632d82bf6f
EC2 Default User <ec2-user@ip-172-31-33-118.ec2.internal>
2023-02-24 07:48:19 +0000

    [add] create actions on admin/order_controller and admin/order_details_controller

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,4 +1,13 @@
 class Admin::OrderDetailsController < ApplicationController
   def update_making_process
+    order_detail = OrderDetail.find(params[:id])
+    order_detail.update(order_detail_params)
+    redirect_to admin_order_path(Order.find(params[:id]))
+  end
+
+  private
+
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status)
   end
 end
