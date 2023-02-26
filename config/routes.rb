811b82b9d8da1f6ffdd4cbd74d53adf6920e6757
EC2 Default User <ec2-user@ip-172-31-33-118.ec2.internal>
2023-02-26 18:52:52 +0000

    [update] minor fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  # ,skip: [:passwords],controllers: {
-  #   registrations: "public/registrations",
-  #   sessions: "public/sessions"
-  # }
+  devise_for :customers,skip: [:passwords],controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
   scope module: :public do
     root to: "homes#top"
     get 'about' => "homes#about",as: "about"
@@ -28,10 +27,9 @@ Rails.application.routes.draw do
     end
     resources :addresses,except:[:new,:show]
   end
-  devise_for :admins
-  # ,skip: [:registrations,:passwords],controllers: {
-  #   sessions: "admin/sessions"
-  # }
+  devise_for :admin,skip: [:registrations,:passwords],controllers: {
+    sessions: "admin/sessions"
+  }
   namespace :admin do
     root to: "homes#top"
     resources :items,except:[:destroy]
