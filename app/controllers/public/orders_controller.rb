aee633750239845b191ace1b4fcd3234a7902595
EC2 Default User <ec2-user@ip-172-31-33-118.ec2.internal>
2023-02-26 08:28:46 +0000

    [add] create public routes, controllers, actions and views

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,19 @@
+class Public::OrdersController < ApplicationController
+  def new
+  end
+  
+  def create
+  end
+
+  def comfirm
+  end
+
+  def complete
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,36 @@ Rails.application.routes.draw do
   #   registrations: "public/registrations",
   #   sessions: "public/sessions"
   # }
-  get "admin" => "admin/homes#top"
+  scope module: :public do
+    root to: "homes#top"
+    get 'about' => "homes#about",as: "about"
+    resources :items,only:[:index,:show]
+    scope :customers do
+      get 'mypage' => 'customers#show',as: "mypage"
+      get 'information/edit' => 'customers#edit',as: "edit_customers"
+      patch 'information' => 'customers#update'
+      get 'confirm' => 'customers#confirm',as: "confirm_customers"
+      patch 'withdraw' => 'customers#withdraw'
+    end
+    resources :cart_items,except:[:new,:show,:edit] do
+      collection do
+        delete "all_destroy"
+      end
+    end
+    resources :orders,except:[:edit,:update,:destroy] do
+      collection do
+        post 'comfirm'
+        get 'complete'
+      end
+    end
+    resources :addresses,except:[:new,:show]
+  end
   devise_for :admins
   # ,skip: [:registrations,:passwords],controllers: {
   #   sessions: "admin/sessions"
   # }
   namespace :admin do
+    root to: "homes#top"
     resources :items,except:[:destroy]
     resources :genres,except:[:new,:show,:destroy]
     resources :customers,except:[:new,:create,:destroy]
