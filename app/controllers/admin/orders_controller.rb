e1ad4fdae16fa2307f4a489baaf91bcf553b96d3
EC2 Default User <ec2-user@ip-172-31-33-118.ec2.internal>
2023-04-04 03:46:56 +0000

    [update]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,6 +5,7 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details.all
+    @total = @order_details.inject(0) { |sum, order_detail| sum + order_detail.sub_total }
   end
 
   def update_status
@@ -12,6 +13,13 @@ class Admin::OrdersController < ApplicationController
     @order.update(order_params)
     flash[:notice] = "注文ステータスを変更しました"
     redirect_to admin_order_path(@order.id)
+
+    if @order.status == "payment_confirmation"
+      @order.order_details.each do |order_detail|
+        order_detail.update(making_status: 1)
+      end
+    end
+
   end
 
   private
